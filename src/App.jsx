19aebc7acc3d5e2d081cf2aa03df334698147d35
Rashid Shahriar <rashidslink@gmail.com>
2023-02-12 11:30:04 +0600

    dw

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Background from './Bg.jsx'
+function App() {
+
+
+  return (
+    <div className="App">
+      <Background />
+    </div>
+  )
+}
+
+export default App
---
+++ src/Bg.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import BG from "./img/tree-bg.png";
+import "./token.css";
+
+const Bg = () => {
+  return (
+    <section id="hero">
+        <div className="relative h-[460px] w-full bg-gradient-to-b from-cyan-500 to-blue-800 bg-cover  bg-center">
+          <div className="tokenbg "></div>
+          <div className="flex md:hidden">
+            <img
+              src={BG}
+              className="w-full h-[460px] object-cover absolute mix-blend-overlay"
+              alt="background"
+            />{" "}
+          </div>
+
+          <div className="flex flex-col min-h-[460px] justify-center items-center gap-y-6 md:gap-y-10">
+            <h1 className="text-[50px] mb-7 text-white font-poppins">
+              Two Tokens
+            </h1>
+          </div>
+        </div>{" "}
+      </section>
+  )
+}
+
+export default Bg
